13b70e81c09c88063509d36daabd39ad453c38d6
recluseZhan <1229233989@qq.com>
2024-09-15 00:43:14 +0800

    no1

--- .readgpa1.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/zjx/tetd-h/readgpa1.mod := printf '%s\n'   readgpa.o | awk '!x[$$0]++ { print("/home/zjx/tetd-h/"$$0) }' > /home/zjx/tetd-h/readgpa1.mod
--- .readgpa1.o.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/zjx/tetd-h/readgpa1.o := ld -m elf_x86_64 -z noexecstack --no-warn-rwx-segments   -r -o /home/zjx/tetd-h/readgpa1.o @/home/zjx/tetd-h/readgpa1.mod 
